d7172a4db1514b7ef5ededfbf775fcf95748d9e6
edomarkosyan <edomarkosyan@gmail.com>
2024-04-28 12:54:48 +0400

    Add nuget packages, start configure sql server

---
+++ VehiclesApi/Data/ApiDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using VehiclesApi.Models;
+
+namespace VehiclesApi.Data
+{
+    public class ApiDbContext : DbContext
+	{
+		public DbSet<Vehicle> Vehicles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+        }
+    }
+}
