63e8bbed2f9d122738e6ef3c77890890bc763702
edomarkosyan <edomarkosyan@gmail.com>
2024-05-01 13:25:41 +0400

    #CHG Create and resolve Vehicle Service, IVehicle interface, create VehiclesController

---
+++ VehiclesApi/Controllers/VehiclesController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using VehiclesApi.Interfaces;
+using VehiclesApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VehiclesApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class VehiclesController : Controller
+    {
+        private IVehicle _vehicleService;
+
+        public VehiclesController(IVehicle vehicleService)
+        {
+            _vehicleService = vehicleService;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public async Task<IEnumerable<Vehicle>> Get()
+        {
+            var vehciles = await _vehicleService.GetAllVehicles();
+            return vehciles;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<Vehicle> Get(int id)
+        {
+            return await _vehicleService.GetVehicleById(id) ;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task Post([FromBody] Vehicle vehicle)
+        {
+            await _vehicleService.AddVehicle(vehicle);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public async Task Put(int id, [FromBody] Vehicle vehicle)
+        {
+            await _vehicleService.UpdateVehicle(id, vehicle);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            await _vehicleService.DeleteVehicle(id);
+        }
+    }
+}
+
---
+++ VehiclesApi/Services/VehicleService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using VehiclesApi.Data;
+using VehiclesApi.Interfaces;
+using VehiclesApi.Models;
+
+namespace VehiclesApi.Services
+{
+    public class VehicleService : IVehicle
+    {
+        private ApiDbContext _dbContext;
+
+        public VehicleService()
+        {
+            _dbContext = new ApiDbContext();
+        }
+
+        public async Task AddVehicle(Vehicle vehicle)
+        {
+            await _dbContext.Vehicles.AddAsync(vehicle);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteVehicle(int id)
+        {
+            var vehicle = await _dbContext.Vehicles.FindAsync(id);
+            _dbContext.Vehicles.Remove(vehicle);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<Vehicle>> GetAllVehicles()
+        {
+            var vehicles = await _dbContext.Vehicles.ToListAsync();
+            return vehicles;
+        }
+
+        public async Task<Vehicle> GetVehicleById(int id)
+        {
+            var vehicle = await _dbContext.Vehicles.FindAsync(id);
+            return vehicle;
+        }
+
+        public async Task UpdateVehicle(int id, Vehicle vehicle)
+        {
+            var vehicleObj = await _dbContext.Vehicles.FindAsync(id);
+
+            vehicleObj.Name = vehicle.Name;
+            vehicleObj.Price = vehicle.Price;
+            vehicleObj.MaxSpeed = vehicle.MaxSpeed;
+            vehicleObj.Length = vehicle.Length;
+            vehicleObj.ImageUrl = vehicle.ImageUrl;
+            vehicleObj.Height = vehicle.Height;
+            vehicleObj.Displacement = vehicle.Displacement;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
